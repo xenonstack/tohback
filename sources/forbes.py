c2dd4f7ad20a6daada4ac51e0fbf9e0c6a679f5b
Abhay Arora <belieflimitless@gmail.com>
2015-10-19 12:36:59 +0530

    Running crawler.

--- sources/forbes.py
+++ sources/forbes.py
@@ -6,6 +6,9 @@
 from scrapy import Spider, Request
 from bs4 import BeautifulSoup
 import urlparse
+import re
+import json
+import time
 
 __author__ = "Abhay Arora ( @BeliefLimitless )"
 __copyright__ = "Copyright (c) 2015 Abhay Arora."
@@ -26,12 +29,22 @@ class Forbes(Spider):
     def parse(self, response):
         html = BeautifulSoup(response.body, 'html.parser')
         links = html.find_all('a')
+        scripts = html.find_all('script')
+        regex = re.compile('(fbs_settings.content)( *)(=)(.*)(;)')
         for word in self.keywords:
             if word in html.title.text.lower():
-                document = dict(
-                    title = html.title.text
-                )
-                yield document
+                for script in scripts:
+                    parsed = regex.findall(script.text)
+                    if len(parsed) > 0:
+                        dataobj = json.loads(parsed[0][3])
+                        if 'content' in dataobj.get('id'):
+                            document = dict(
+                                source = 'forbes',
+                                uid = dataobj.get('id'),
+                                crawl_time = time.time(),
+                                data = parsed[0][3]
+                            )
+                            yield document
         for word in self.search_domain:
             for link in links:
                 if link.get('href') != None and word in link.get('href'):
